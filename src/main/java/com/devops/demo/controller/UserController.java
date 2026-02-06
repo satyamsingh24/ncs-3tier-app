f14d47be3a917b5b19f94a85f5e8f477aac770b8
EC2 Default User <ec2-user@ip-10-0-2-176.ap-south-1.compute.internal>
2026-02-06 18:02:35 +0000

    Initial commit from EC2

--- src/main/java/com/devops/demo/controller/UserController.java
+++ src/main/java/com/devops/demo/controller/UserController.java
@@ -2,26 +2,27 @@ package com.devops.demo.controller;
 
 import com.devops.demo.model.User;
 import com.devops.demo.repository.UserRepository;
-import org.springframework.web.bind.annotation.*;  // RestController, RequestMapping, PostMapping, GetMapping, RequestBody
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:9095")
+@CrossOrigin(origins = "*")   // ✅ Frontend public EC2 ke liye
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
     private final UserRepository userRepository;
 
+    // ✅ Constructor Injection (Best Practice)
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    // ✅ Save user in MongoDB
     @PostMapping("/register")
     public User register(@RequestBody User user) {
         return userRepository.save(user);
     }
 
+    // ✅ Fetch all users from MongoDB
     @GetMapping("/users")
     public Iterable<User> getUsers() {
         return userRepository.findAll();
